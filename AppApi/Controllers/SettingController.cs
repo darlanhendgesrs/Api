624e53a34d7ee8577daa54b95d149bc40af125e5
Darlan Hendges <36051279+darlanhendgesrs@users.noreply.github.com>
2023-11-28 10:26:18 +0100

    Add files via upload
    First commit.

---
+++ AppApi/Controllers/SettingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingController : ControllerBase
+    {
+        private readonly ILogger<SettingController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public SettingController(ILogger<SettingController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_configuration["ClientSecret"]);
+        }
+
+    }
+}
